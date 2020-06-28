0fec354b966bacc974cc2331338f3f692fa1e428
LDoubleZhi <email@example.com>
2020-06-28 10:15:52 +0800

    review java basic

---
+++ src/BasicReview/BasicReview.java
@@ -0,0 +1,12 @@
+package BasicReview;
+
+/**
+ * @author : liulinzhi
+ * @date: 2020/06/28/10:14
+ * @description:
+ */
+public class BasicReview {
+    public static void main(String[] args) {
+
+    }
+}
